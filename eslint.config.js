d96f20c243c42fe4e3ddb5a993fa7f43200d8c79
alistair <alistairnds@gmail.com>
2025-06-24 15:19:48 -0700

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import json from '@eslint/json';
-import markdown from '@eslint/markdown';
-import css from '@eslint/css';
+// import json from '@eslint/json';
+// import markdown from '@eslint/markdown';
+// import css from '@eslint/css';
 import { defineConfig } from 'eslint/config';
+import { includeIgnoreFile } from '@eslint/compat';
+import { fileURLToPath } from 'node:url';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import eslintConfigPrettier from 'eslint-config-prettier';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default defineConfig([
   {
@@ -19,40 +23,41 @@ export default defineConfig([
     languageOptions: { globals: globals.browser }
   },
   tseslint.configs.recommended,
-  {
-    files: ['**/*.json'],
-    plugins: { json },
-    language: 'json/json',
-    extends: ['json/recommended']
-  },
-  {
-    files: ['**/*.jsonc'],
-    plugins: { json },
-    language: 'json/jsonc',
-    extends: ['json/recommended']
-  },
-  {
-    files: ['**/*.json5'],
-    plugins: { json },
-    language: 'json/json5',
-    extends: ['json/recommended']
-  },
-  {
-    files: ['**/*.md'],
-    plugins: { markdown },
-    language: 'markdown/gfm',
-    extends: ['markdown/recommended']
-  },
-  {
-    files: ['**/*.css'],
-    plugins: { css },
-    language: 'css/css',
-    extends: ['css/recommended']
-  },
+  // {
+  //   files: ['**/*.json'],
+  //   plugins: { json },
+  //   language: 'json/json',
+  //   extends: ['json/recommended']
+  // },
+  // {
+  //   files: ['**/*.jsonc'],
+  //   plugins: { json },
+  //   language: 'json/jsonc',
+  //   extends: ['json/recommended']
+  // },
+  // {
+  //   files: ['**/*.json5'],
+  //   plugins: { json },
+  //   language: 'json/json5',
+  //   extends: ['json/recommended']
+  // },
+  // {
+  //   files: ['**/*.md'],
+  //   plugins: { markdown },
+  //   language: 'markdown/gfm',
+  //   extends: ['markdown/recommended']
+  // },
+  // {
+  //   files: ['**/*.css'],
+  //   plugins: { css },
+  //   language: 'css/css',
+  //   extends: ['css/recommended']
+  // },
   eslintPluginAstro.configs.recommended,
   eslintPluginAstro.configs['jsx-a11y-recommended'],
   eslintConfigPrettier,
   {
     rules: {}
-  }
+  },
+  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns')
 ]);
